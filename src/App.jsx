a26e376010f2d17e291703f075441caf830ee0e0
Anderson Marlon <anderson18.marlon@gmail.com>
2022-10-25 11:07:17 -0300

    Initial commit
    - App works in PT-BR and EN-US

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
+
+export default function AnyComponent() {
+    const [language, setLanguage] = useState('pt-BR');
+    const {
+        error,
+        interimResult,
+        isRecording,
+        results,
+        startSpeechToText,
+        stopSpeechToText,
+    } = useSpeechToText({
+        continuous: true,
+        crossBrowser: true,
+        googleApiKey: 'AIzaSyALe6mlG6RJScEdlBsz8E2CrNn2dSsboJ4',
+        useLegacyResults: false,
+        speechRecognitionProperties: {
+            lang: language,
+            interimResults: true
+        }
+    });
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+  return (
+    <div>
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
+      <label>Language: </label>
+      <select onChange={event => (setLanguage(event.target.value))}>
+        <option value="en-US">English</option>
+        <option value="pt-BR">Portuguese</option>
+      </select>
+    </div>
+  );
+}
---
+++ src/components/HomeDetails.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+const HomeDetails = styled.div `
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        label {
+            margin: 1rem 0;
+        }
+
+        input {
+            margin: 1rem 0;
+        }
+
+        button {
+            margin: 1rem 0;
+        }
+    }
+`
+
+export default HomeDetails
